097181572b3c85b0e871a4dba167cbebd23d0da8
chienlm-1082 <le.minh.chien@sun-asterisk.com>
2021-02-01 12:50:07 +0700

    add threshold white mask image

--- completedModel.py
+++ completedModel.py
@@ -21,9 +21,20 @@ class CompletedModel():
         self.add_boxes = None
         self.home_boxes = None
         self.cropped_image = None
-    
+        self.field_dict = dict()
+        self.mask = None
+        self.id_card = None
+        self.ratio = None
+
+
+
     def segment_id(self, image):
         mask, id_card = segment_mask(image)
+        self.ratio = caculate_ratio_white_mask(mask)
+        self.id_card = cv2.bitwise_and(id_card, id_card, mask)
+        self.mask = mask
+        if self.ratio > 0.9:
+            return id_card
         lines = line_detection(mask)
         segmented = segment_by_angle_kmeans(lines)
         intersections = segmented_intersections(segmented)
@@ -32,7 +43,6 @@ class CompletedModel():
         dst_points = [[0, 0], [500, 0], [500, 300], [0, 300]]
         source_points = corner
         dst = perspective_transform(id_card, source_points, dest_points=dst_points, out_size=(500, 300))
-
         return dst
     
     def detect_text(self, image):
@@ -43,7 +53,7 @@ class CompletedModel():
 
     
     def text_recognition(self, image):
-        field_dict = dict()
+        self.field_dict = dict()
 
         def crop_and_recog(boxes):
             crop = []
@@ -68,12 +78,12 @@ class CompletedModel():
         result = self.text_recognition_model.predict_on_batch(np.array(list_ans))
         end1 = time.time()
         print("predicted time: ", end1 - start1)
-        field_dict['id'] = result[0]
-        field_dict['name'] = ' '.join(result[1:len(self.name_boxes) + 1])
-        field_dict['birth'] = result[len(self.name_boxes) + 1]
-        field_dict['home'] = ' '.join(result[len(self.name_boxes) + 2: -len(self.home_boxes)])
-        field_dict['add'] = ' '.join(result[-len(self.home_boxes):])
-        print(field_dict)
+        self.field_dict['id'] = result[0]
+        self.field_dict['name'] = ' '.join(result[1:len(self.name_boxes) + 1])
+        self.field_dict['birth'] = result[len(self.name_boxes) + 1]
+        self.field_dict['home'] = ' '.join(result[len(self.name_boxes) + 2: -len(self.home_boxes)])
+        self.field_dict['add'] = ' '.join(result[-len(self.home_boxes):])
+        print(self.field_dict)
 
     def predict(self, image):
         self.cropped_image = self.segment_id(image)
