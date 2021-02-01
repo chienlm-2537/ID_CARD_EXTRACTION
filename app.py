5fa29f3aa6b1196921f40079d7866505afcee2ee
chienlm-1082 <le.minh.chien@sun-asterisk.com>
2021-02-01 11:31:46 +0700

    them mot so tinh nang hien thi

--- app.py
+++ app.py
@@ -3,31 +3,86 @@ import streamlit as st
 from PIL import Image
 import cv2
 import numpy as np
+from completedModel import CompletedModel
+import time
+
+
 
 st.title('--- ID CARD EXTRACTION ---')
 st.write('\n')
 
-# image = Image('text_detection_faster/test.png')
-# show = st.image(image, use_column_width=False, width=300)
-
-# image = cv2.imread('text_detection_faster/test.png')
-# show = st.image(image, use_column_width=False, width=300)
-
-
+@st.cache(persist = True)
+def load_model():
+  model = CompletedModel()
+  return model
 
 st.sidebar.title("Upload Image")
+model = CompletedModel()
+# st.set_option('deprecation.showfileUploaderEncoding', False)
+def main():
+  # model = load_model()
+  uploaded_file = st.sidebar.file_uploader("",type=['png', 'jpg', 'jpeg'])
+  if uploaded_file is not None:
+      print(type(uploaded_file))
+      file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+      test = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+      show = st.image(test, use_column_width=True, width=300, channels="BGR")
+  if st.sidebar.button("Click here to Extract Information"):
+    if uploaded_file is None:
+      st.sidebar.write("Please upload an Image to Extract Information")
+    else:
+      try:
+        print("start extraction")
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        test = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        start = time.time()
+        model.predict(test)
+        end = time.time()
+        show1 = st.image(model.mask, use_column_width=True, width=300, caption="Mask image")
+        show3 = st.image(model.id_card, use_column_width=True, width=300, channels="BGR", caption="ID_Card image")
+        show2 = st.image(model.cropped_image, use_column_width=True, width=300, channels="BGR", caption="detect field")
+        st.write("Time processing: {} s".format(round(end-start)))
+        st.write("\n")
+        st.write("White mask ratio {}".format(model.ratio))
+        print(model.field_dict.keys())
+        st.write("ID NUMBER: {}".format(model.field_dict['id']))
+        st.write("\n")
+        st.write("NAME: {}".format(str(model.field_dict["name"]).upper()))
+        st.write("\n")
+        st.write("BIRTH: {}".format(model.field_dict["birth"]))
+        st.write("\n")
+        st.write("HOME: {}".format(model.field_dict["home"]))
+        st.write("\n")
+        st.write("ADDRESS: {}".format(model.field_dict["add"]))
+        pass
+      except:
+        st.write("ERROR WHEN EXTRACT INFORMATION FROM IDCARD")
+        pass
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+      # try:
+      #   print("start extraction")
+      #   file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+      #   test = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+      #   start = time.time()
+      #   model.predict(test)
+      #   end = time.time()
+      #   show1 = st.image(model.cropped_image, use_column_width=True, width=300, channels="BGR")
+      #   st.write("Time processing: {} s".format(round(end-start)))
+      #   print(model.field_dict.keys())
+      #   st.write("ID NUMBER: {}".format(model.field_dict['id']))
+      #   st.write("\n")
+      #   st.write("NAME: {}".format(model.field_dict["name"]))
+      #   st.write("\n")
+      #   st.write("BIRTH: {}".format(model.field_dict["birth"]))
+      #   st.write("\n")
+      #   st.write("HOME: {}".format(model.field_dict["home"]))
+      #   st.write("\n")
+      #   st.write("ADDRESS: {}".format(model.field_dict["add"]))
+      # except:
+      #   st.write("ERROR WHEN EXTRACT INFORMATION FROM IDCARD")
+      #   pass
 
-uploaded_file = st.sidebar.file_uploader("",type=['png', 'jpg', 'jpeg'])
-
-if st.sidebar.button("Click here to Extract Information"):
-  if uploaded_file is None:
-    st.sidebar.write("Please upload an Image to Extract Information")
-  else:
-    print(type(uploaded_file))
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    test = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    st.write(str(type(test)))
-    show = st.image(test, use_column_width=True, width=300, channels="BGR")
-    cv2.imwrite('chien.png', test)
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    # cv2.imwrite('chien.png', test)
